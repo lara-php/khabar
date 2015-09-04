3b0de9aef507f6b16bb0abebe2c23441b5e3ef77
m-ziarati <mahdiehaghaziarati@yahoo.com>
2015-09-04 18:19:37 +0430

    khabar

---
+++ app/views/index.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.master')
+
+@stop
+
+@section('content')
+<div class="category-news">
+	<div class="news">
+		<div class="title">تکنولوژی</div>
+
+		
+
+		<div class="category-image">
+			
+		</div>
+		<div class="text">
+			<h4><a href=""></a></h4>
+			<div class="subtitle"></div>
+		</div>
+
+		
+
+	</div>
+
+	<div class="news">
+		<div class="title">جهان</div>
+
+		
+		<div class="category-image">
+			
+		</div>
+		<div class="text">
+			<h4><a href=""></a></h4>
+			<div class="subtitle"></div>
+		</div>
+
+		
+	</div>
+	<div class="news">
+		<div class="title">فرهنگی</div>
+		
+		<div class="category-image">
+			
+		</div>
+		<div class="text">
+			<h4><a href=""></a></h4>
+			<div class="subtitle"></div>
+		</div>
+		
+	</div>
+	<div class="news">
+		<div class="title">ورزشی</div>
+		
+		<div class="category-image">
+			
+		</div>
+		<div class="text">
+			<h4><a href=""></a></h4>
+			<div class="subtitle"></div>
+		</div>
+	
+	</div>
+	<div class="clearfix"></div>
+	</div>
+@stop
