c318d393e0c6c89c2203b4818c743a76004bab46
m-ziarati <mahdiehaghaziarati@yahoo.com>
2015-09-11 23:06:21 +0430

    c

--- app/routes.php
+++ app/routes.php
@@ -21,11 +21,38 @@ Route::get('index',['as'=> 'index' ,function()
 	return View::make('index');
 }]);
 
-Route::get('cms',['as'=> 'cms' , function()
-{
-	return View::make('layouts.cms');
+Route::get('loginError', ['as' => 'loginError' , function(){
+	return View::make('user.loginError');
 }]);
 
+
+
+Route::post('login', ['before' => 'csrf', 'as'=> 'login', function(){
+
+	
+	$remember = (Input::has('remember'))  ? true : false;
+	
+	
+	if(Auth::attempt(['username'=>Input::get('username'),'password'=>Input::get('password') ],$remember))
+	{
+		return  Redirect::route('cms.article.list');
+	}
+
+	Session::flash('loginError', true);
+
+	return Redirect::route('loginError');
+		
+}]);
+
+
+
+Route::get('logout', ['as' => 'logout', function(){
+	Auth::logout();
+
+	return Redirect::route('index');
+}]);
+
+<<<<<<< HEAD
 Route::get('category/{id}', ['as'=> 'category' ,function($id)
 {
 	$category=Category::find($id);
@@ -38,6 +65,21 @@ Route::get('category/{id}', ['as'=> 'category' ,function($id)
 					->with('articles',$articles );
 					
 }]);
+=======
+Route::get('user/passwordRemind', ['as'=> 'password.remind', function(){
+
+	return View::make('user.passwordRemind');
+}]);
+
+Route::post('RemindersController', ['as' => 'RemindersController', 'uses' => 'RemindersController@postRemind']);
+
+Route::get('user/passwordReset', ['as'=> 'password.reset', function(){
+
+	return View::make('user.passwordReset');
+}]);
+Route::post('RemindersController', ['as' => 'RemindersController', 'uses' => 'RemindersController@postReset']);
+
+>>>>>>> parent of e8deadc... cms route
 
 Route::group(['prefix' => 'cms/article/'], function()
 {
@@ -46,7 +88,7 @@ Route::group(['prefix' => 'cms/article/'], function()
 
 Route::get('', ['as' => 'article.list' , function(){
 
-	$query = Article::select('*');
+	$query = Content::select('*');
 
 		foreach (Input::only('id', 'date', 'title', 'publish') as $key => $value)
 		{
@@ -65,6 +107,111 @@ Route::get('', ['as' => 'article.list' , function(){
 }]);
 
 
+/* route article.add */
 
+Route::get('add', ['as' => 'article.add' , function(){
+
+	return View::make('cms.article.add');									
+}]);
+
+/* route article.insert.validation */
+
+Route::post('insert', ['as' => 'article.insert' , function(){
+
+	$rules = [
+			  'date'   =>'required|max:255|date',
+			  'title'  =>'required|max:255|unique:article',
+			  'publish'=>'required|boolean'
+			  ];
+
+	$validation = Validator::make(Input::all(),$rules);
+
+	if($validation -> fails())
+	{
+		Input::flash();
+
+		return Redirect::route('article.add')
+											->withErrors($validation);
+	}
+
+	$article = new Article;
+	$article ->date    =Input::get('date');
+	$article ->title   =Input::get('title');
+	$article ->publish =Input::get('publish');
+	$article ->save();
+
+	foreach (Input::get('categoryId') as $categoryId ) 
+	{
+		$categoryArticle =  new categoryArticle ;
+		$categoryArticle ->articleId = $article->id;
+		$categoryArticle ->categoryId = $categoryId;
+		$categoryArticle ->save();
+		
+	}
+	
+	Session::flash('inserted', true);
+
+	return Redirect::route('article.list');
+									
+}]);
+/* route article.edit */
+
+Route::get('edit/{id}',['as' =>'article.edit' , function($id){
+
+	$article = Article::find($id);
+
+	return View::make('cms/article.edit')
+									->with('article' , $article);
+
+
+}]);
+
+/* route article.update */
+
+Route::post('update', ['as' => 'article.update' , function(){
+
+	$rules = [
+			  'id'   =>'required|exists:article,id',
+			  'date'   =>'required|date',
+			  'title'  =>'required|max:255',
+			  'publish'=>'required'
+			  ];
+
+	$validation = Validator::make(Input::all(),$rules);
+
+	if($validation -> fails())
+	{
+		Input::flash();
+
+		return Redirect::route('article.edit', Input::get('id'))
+											->withErrors($validation);
+	}
+
+	$article = Article::find(Input::get('id'));
+	$article ->date    =Input::get('date');
+	$article ->title   =Input::get('title');
+	$article ->publish =Input::get('publish');
+	$article ->save();
+
+	Session::flash('updated', true);
+
+	return Redirect::route('article.list');
+									
+}]);
+
+/* route article.delete */
+
+Route::get('delete/{id}', ['as'=>'article.delete' , function($id){
+
+	$article= Article::find($id);
+	$article->delete();
+	Session::flash('deleted', true);
+
+	return Redirect::route('article.list');
+
+
+
+
+}]);
 
 });
